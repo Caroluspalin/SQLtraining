a127267a7c3c9840c50d21f7b472b2f4f67e2739
Carolus <caroluspalin@icloud.com>
2026-01-18 22:06:00 +0200

    More training

--- training1.sql
+++ training1.sql
@@ -8,3 +8,20 @@ SELECT AVG(Hinta) AS AveragePrice FROM autot;
 SELECT AVG(Hinta) AS ToyotaAVG 
 FROM autot
 WHERE Merkki = 'Toyota';
+
+SELECT Merkki, COUNT(Hinta)
+FROM autot
+GROUP BY merkki;
+
+SELECT * FROM autot
+ORDER BY Hinta;
+
+SELECT * FROM autot
+ORDER BY Hinta DESC ;
+
+SELECT * FROM autot
+ORDER BY Merkki ASC, Hinta DESC;
+
+SELECT * FROM autot
+Where Vuosi > 2015 
+ORDER BY Hinta DESC;
